e777bbddb3b5217940f03b3883ff0863c5261892
Sean Reid <sean@torchcodelab.com>
2022-06-09 17:16:54 -0400

    Updated amplify per README, installed missing libraries

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "rnconfinabox58de14db": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "api": {
+        "rnconfinabox": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
